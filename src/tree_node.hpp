735cea61632c0b0c5ce94c72c05939c4a4581f5f
pedrohmx <pedrohx@yahoo.com.br>
2022-06-22 15:32:54 -0300

    Added tree_node header

---
+++ src/tree_node.hpp
@@ -0,0 +1,20 @@
+#ifndef TREE_NODE_INCLUDED_HPP
+#define TREE_NODE_INCLUDED_HPP
+
+#include <memory>
+
+namespace paa {
+namespace t3 {
+
+template<typename T>
+struct b_tree_node_t
+{
+    b_tree_node_t(const T value) : data(value){};
+    T data;
+    std::shared_ptr<b_tree_node_t<T>>  lnode, rnode;
+};
+
+}} // namespace paa::t3
+
+
+#endif
