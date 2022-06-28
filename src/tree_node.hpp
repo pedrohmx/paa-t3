239f73918498a50383433c7ab2d9abb912733247
pedrohmx <pedrohx@yahoo.com.br>
2022-06-28 10:59:34 -0300

    Added reference to parent node

--- src/tree_node.hpp
+++ src/tree_node.hpp
@@ -11,7 +11,8 @@ struct b_tree_node_t
 {
     b_tree_node_t(const T value) : data(value){};
     T data;
-    std::shared_ptr<b_tree_node_t<T>>  lnode, rnode;
+    std::shared_ptr<b_tree_node_t<T>> parent;
+    std::shared_ptr<b_tree_node_t<T>> lnode, rnode;
 };
 
 }} // namespace paa::t3
