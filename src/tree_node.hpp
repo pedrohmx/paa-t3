4d490f65d90353200161045a59fdb0e46b33e807
pedrohmx <pedrohx@yahoo.com.br>
2022-06-28 11:11:52 -0300

    Added `height()` and `balance()`

--- src/tree_node.hpp
+++ src/tree_node.hpp
@@ -10,6 +10,16 @@ template<typename T>
 struct b_tree_node_t
 {
     b_tree_node_t(const T value) : data(value){};
+    long height() const {
+        long lh = (!lnode) ? 0 : lnode->height();
+        long rh = (!rnode) ? 0 : rnode->height();
+        return (lh > rh) ? lh + 1 : rh + 1;
+    };
+    long balance() const {
+        long lh = (!lnode) ? 0 : lnode->height();
+        long rh = (!rnode) ? 0 : rnode->height();
+        return rh - lh;
+    };
     T data;
     std::shared_ptr<b_tree_node_t<T>> parent;
     std::shared_ptr<b_tree_node_t<T>> lnode, rnode;
