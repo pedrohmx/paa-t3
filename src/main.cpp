9efb711f31082f898f6a1ac6b49bc3c56507939d
pedrohmx <pedrohx@yahoo.com.br>
2022-07-08 11:33:05 -0300

    Added timer and export to file

--- src/main.cpp
+++ src/main.cpp
@@ -2,7 +2,7 @@
 #include <fstream>
 #include <string>
 #include <vector>
-#include <cstdint>
+#include <chrono>
 
 #include "argparse/argparse.hpp"
 
@@ -81,31 +81,75 @@ auto main(int argc, char *argv[]) -> int {
 
     // write results to output
     std::ofstream ofs_output(str_out);
+    ofs_output << "BST_BUILD_T | BST_BUILD_C | BST_QUERY_T | BST_QUERY_C | AVL_BUILD_T | AVL_BUILD_C | AVL_QUERY_T | AVL_QUERY_C" << endl;
+    ofs_output << std::fixed;
+
+    // timing
+    using clock = std::chrono::high_resolution_clock;
+    using std::chrono::duration_cast;
+    using milli = std::chrono::milliseconds;
+    using nano = std::chrono::nanoseconds;
 
     // run tests
     for (int r = 0; r < runs; r++) {
-        cout << "Running [" << r+1 << '/' << runs << "]" << endl;
+        cout << "Running [" << r+1 << '/' << runs << "]"; cout.flush();
 
         BST bst;
+        auto bst_build_t1 = clock::now();
         for (auto i : in_build) {
             bst.insert(i);
         }
+        auto bst_build_t2 = clock::now();
+        cout << " ."; cout.flush();
+
+        auto bst_query_t1 = clock::now();
         for (auto i : in_query) {
             bst.search(i);
         }
+        auto bst_query_t2 = clock::now();
+        cout << " ."; cout.flush();
 
         AVL avl;
+        auto avl_build_t1 = clock::now();
         for (auto i : in_build) {
             avl.insert(i);
         }
+        auto avl_build_t2 = clock::now();
+        cout << " ."; cout.flush();
+
+        auto avl_query_t1 = clock::now();
         for (auto i : in_query) {
             avl.search(i);
         }
+        auto avl_query_t2 = clock::now();
+        cout << " ." << endl;
 
-        cout << "BST: B-> " << bst.get_insertion_cmp_count() << " Q-> " << bst.get_search_cmp_count() << endl;
-        cout << "AVL: B-> " << avl.get_insertion_cmp_count() << " Q-> " << avl.get_search_cmp_count() << endl;
+        std::chrono::duration<double, std::milli> bst_build_span = bst_build_t2 - bst_build_t1;
+        std::chrono::duration<double, std::milli> bst_query_span = bst_query_t2 - bst_query_t1;
+        std::chrono::duration<double, std::milli> avl_build_span = avl_build_t2 - avl_build_t1;
+        std::chrono::duration<double, std::milli> avl_query_span = avl_query_t2 - avl_query_t1;
+
+        // "BST_BUILD_T | BST_BUILD_C | BST_QUERY_T | BST_QUERY_C | AVL_BUILD_T | AVL_BUILD_C | AVL_QUERY_T | AVL_QUERY_C"
+        cout         << bst_build_span
+            << " | " << bst.get_insertion_cmp_count()
+            << " | " << bst_query_span
+            << " | " << bst.get_search_cmp_count()
+            << " | " << avl_build_span
+            << " | " << avl.get_insertion_cmp_count()
+            << " | " << avl_query_span
+            << " | " << avl.get_search_cmp_count()
+            << endl;
+
+        ofs_output   << bst_build_span
+            << " | " << bst.get_insertion_cmp_count()
+            << " | " << bst_query_span
+            << " | " << bst.get_search_cmp_count()
+            << " | " << avl_build_span
+            << " | " << avl.get_insertion_cmp_count()
+            << " | " << avl_query_span
+            << " | " << avl.get_search_cmp_count()
+            << endl;
     }
-    
 
     return 0;
 }
